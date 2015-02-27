9411d118c037fed598f43cc064295bc3e32c5bc7
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-27 14:06:07 +0100

    Ajout dans Graphe.java : ajout d'un main pour tester le résultat de l'initialisation des listes de successeurs.

--- src/Graphe.java
+++ src/Graphe.java
@@ -118,4 +118,15 @@ public class Graphe {
         System.out.println(result);
     }
     
+    public static void main (String[] args) {
+        String[] dico3court = {
+           "gag", "gai", "gaz", "gel", "gks", "gin",
+           "gnu", "glu", "gui", "guy", "gre", "gue",
+           "ace", "acm", "agi", "ait", "aie", "ail",
+           "air", "and", "alu", "ami", "arc", "are",
+           "art", "apr", "avr", "sur", "mat", "mur" } ;
+        Graphe g = new Graphe (dico3court) ;
+        g.lettreQuiSaute () ;
+        g.display();
+      }
 }
