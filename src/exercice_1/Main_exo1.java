ad171e5b5a2c2df63ca9162faa1a84ce9461bac8
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-10 19:16:59 +0100

    Ajout dans Main_exo1.java : Application du jeu de la lettre qui saute sur les trois dictionnaires de Dicos.java et affichage des résultats.
    Les résultats sont valides. Les dictionnaires contiennent respectivement des mots de trois, quatre et cinq lettres. Le calcul détermine correctement
    les mots voisins d'une lettre pour chacun d'eux.

--- src/exercice_1/Main_exo1.java
+++ src/exercice_1/Main_exo1.java
@@ -1,5 +1,25 @@
 package exercice_1;
 
 public class Main_exo1 {
+    
+    public static void main(String[] args) {
+        
+        Graphe graphe_dico3 = new Graphe(Dicos.dico3);
+        graphe_dico3.lettreQuiSaute();
+        System.out.println("Dictionnaire 3");
+        graphe_dico3.display();
+        
+        Graphe graphe_dico4 = new Graphe(Dicos.dico4);
+        graphe_dico4.lettreQuiSaute();
+        System.out.println("Dictionnaire 4");
+        graphe_dico4.display();
+        
+        Graphe graphe_dico5 = new Graphe(Dicos.dico5);
+        graphe_dico5.lettreQuiSaute();
+        System.out.println("Dictionnaire 5");
+        graphe_dico5.display();
+    
+    
+    }
 
 }
