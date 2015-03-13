67b3fde7ecde04b9e5a354f7b021afd1dbfb0036
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-13 12:01:22 +0100

    Définition du main de l'exercice 3.

--- src/exercice_3/Main_exo3.java
+++ src/exercice_3/Main_exo3.java
@@ -1,10 +1,17 @@
 package exercice_3;
 
+import dictionnaires.Dicos;
+import exercice_3.Graphe;
+
 
 public class Main_exo3 {
 
     
     public static void main(String[] args) {
-       
+        Graphe graphe_dico4 = new Graphe(Dicos.dico4);
+        graphe_dico4.lettreQuiSaute();
+        System.out.println("Dictionnaire 4");
+        graphe_dico4.visit();
+        graphe_dico4.chemin("lion", "peur");
     }
 }
