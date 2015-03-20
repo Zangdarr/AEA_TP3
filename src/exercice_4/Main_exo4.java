e48a4c905013af1c5705c9493ba02685aaa3c1eb
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-20 12:06:51 +0100

    exo4_main : lancement de l'algo de plus court chemin entre duex noeuds.

--- src/exercice_4/Main_exo4.java
+++ src/exercice_4/Main_exo4.java
@@ -1,11 +1,21 @@
 package exercice_4;
 
+import dictionnaires.Dicos;
+import exercice_4.Graphe;
+
 
 
 public class Main_exo4 {
 
     
     public static void main(String[] args) {
-        System.out.println("Main_exo_4 : Not yet implemented");
+        Graphe graphe_dico4 = new Graphe(Dicos.dico4);
+        graphe_dico4.lettreQuiSaute();
+        System.out.println("Dictionnaire 4\n\nBFS : ");
+        graphe_dico4.chemin_bfs("lion", "peur");
+        
+        System.out.println("Amelioration de l'exo 3 : ");
+        
+        graphe_dico4.exo4_chemin_plus_court("lion", "peur");
     }
 }
